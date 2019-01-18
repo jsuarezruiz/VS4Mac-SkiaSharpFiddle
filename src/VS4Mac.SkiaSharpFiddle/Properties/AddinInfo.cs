6fc459a1c21951efe43470ea008acc88366ffed4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-01-18 19:14:54 +0100

    Updated addin info

--- src/VS4Mac.SkiaSharpFiddle/Properties/AddinInfo.cs
+++ src/VS4Mac.SkiaSharpFiddle/Properties/AddinInfo.cs
@@ -1,13 +1,20 @@
-﻿using Mono.Addins;
+﻿using System;
+using System.Runtime.InteropServices;
+using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
     "VS4Mac.SkiaSharpFiddle",
     Namespace = "VS4Mac.SkiaSharpFiddle",
-    Version = "0.1"
+    Version = "0.1",
+    Category = "IDE extensions"
 )]
 
-[assembly: AddinName("VS4Mac.SkiaSharpFiddle")]
+[assembly: AddinName("SkiaSharp Fiddle")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("VS4Mac.SkiaSharpFiddle")]
+[assembly: AddinDescription("SkiaSharp Fiddle is a SkiaSharp playground.")]
 [assembly: AddinAuthor("Javier Suárez Ruiz")]
+[assembly: AddinUrl("https://github.com/jsuarezruiz/VSMac-SkiaSharpFiddle")]
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
